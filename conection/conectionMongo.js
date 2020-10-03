d7c40500184ea2521da67b9c26cdc0698067e5fa
Celestial Blink <kkrakenbreaker@gmail.com>
2020-10-03 18:55:04 -0500

    avance

--- conection/conectionMongo.js
+++ conection/conectionMongo.js
@@ -4,7 +4,7 @@ let urli="mongodb://127.0.0.1:27017/celestial";
 
 let urlAtlas=`mongodb+srv://take:${process.env.CB_DB_PASS}@cluster0.gt8zx.mongodb.net/<celestial>?retryWrites=true&w=majority`;
 
-let myConnection=mongoose.connect(urlAtlas,{
+let myConnection=mongoose.connect(urli,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 });
